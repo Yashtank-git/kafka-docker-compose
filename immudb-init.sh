2bda31f804a9868fcdd4c18790fb8c3437a15ca0
Yash Tank <tankyash180@gmail.com>
2024-07-12 11:15:56 +0000

    add files

---
+++ immudb-init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt update
+apt install curl jq -y
+
+TOKEN=$(curl --location --request POST 'immudb-api:3323/login' \
+--header 'Authorization;' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "user": "aW1tdWRi",
+    "password": "aW1tdWRi"
+}' |  jq -r .token)
+
+
+curl -X POST \
+  http://immudb-api:3323/db/defaultdb/create \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: '$TOKEN'' \
+  -d '{"databaseName":"dataverse"}'
+
+
+curl -X POST \
+  http://immudb-api:3323/user \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: '$TOKEN'' \
+  -d '{"user":"ZGF0YXZlcnNl","password":"REFUQV8xMjM0","permission":254,"database":"dataverse"}'
+
+sleep infinity
+
